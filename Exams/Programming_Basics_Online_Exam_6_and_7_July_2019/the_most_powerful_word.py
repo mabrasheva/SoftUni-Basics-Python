d27f54121b75cc9e185f8e720b01f357d1d73fd9
Maria Mihaleva <mabrasheva1@gmail.com>
2022-07-13 23:02:01 +0300

    added the_most_powerful_word.py

---
+++ Exams/Programming_Basics_Online_Exam_6_and_7_July_2019/the_most_powerful_word.py
@@ -0,0 +1,24 @@
+word = input()
+the_most_powerful_word = ""
+max_sum_letters = 0
+
+while word != "End of words":
+    sum_letters = 0
+    for letter in word:
+        sum_letters += ord(letter)
+
+    if word[0] == "a" or word[0] == "e" or word[0] == "i" \
+            or word[0] == "o" or word[0] == "u" or word[0] == "y" \
+            or word[0] == "A" or word[0] == "E" or word[0] == "I" \
+            or word[0] == "O" or word[0] == "U" or word[0] == "Y":
+        sum_letters *= len(word)
+    else:
+        sum_letters = sum_letters / len(word)
+
+    if max_sum_letters <= sum_letters:
+        max_sum_letters = sum_letters
+        the_most_powerful_word = word
+
+    word = input()
+
+print(f"The most powerful word is {the_most_powerful_word} - {max_sum_letters}")
